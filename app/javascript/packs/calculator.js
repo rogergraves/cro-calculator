d7c933e874fa3c5b54a202700d19565b4bd79bfa
Roger Graves <rogergraves@gmail.com>
2020-04-29 19:25:33 -0700

    Improve how the results are displayed

--- app/javascript/packs/calculator.js
+++ app/javascript/packs/calculator.js
@@ -1,7 +1,5 @@
 
 calculate_cro = () => {
-  console.log("running calculate_cro");
-
   let weekly_ad_spend = parseFloat($('#ad_spend').val());
   let link_clicks = parseFloat($('#link_clicks').val());
   let cvr = parseFloat($('#cvr').val()) / 100.0;
@@ -9,7 +7,7 @@ calculate_cro = () => {
   let denominator = link_clicks * cvr;
 
   if((link_clicks * cvr) > 0) {
-    $('#cac').val(Math.round(100 * weekly_ad_spend / (link_clicks * cvr)) / 100);
+    $('#cac').text(Math.round(100 * weekly_ad_spend / (link_clicks * cvr)) / 100);
   }
 };
 
