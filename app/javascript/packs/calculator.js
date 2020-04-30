9637a49d58ed9d91b9e5cdb891b490de242e728c
Roger Graves <rogergraves@gmail.com>
2020-04-29 19:12:53 -0700

    Build calculator and add jQuery for support

---
+++ app/javascript/packs/calculator.js
@@ -0,0 +1,18 @@
+
+calculate_cro = () => {
+  console.log("running calculate_cro");
+
+  let weekly_ad_spend = parseFloat($('#ad_spend').val());
+  let link_clicks = parseFloat($('#link_clicks').val());
+  let cvr = parseFloat($('#cvr').val()) / 100.0;
+
+  let denominator = link_clicks * cvr;
+
+  if((link_clicks * cvr) > 0) {
+    $('#cac').val(Math.round(100 * weekly_ad_spend / (link_clicks * cvr)) / 100);
+  }
+};
+
+$(document).ready(function() {
+  calculate_cro();
+});
